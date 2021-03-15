33a7697ddd9766f3c40e5a4755f3a52a24f30792
Idan Shoshana <idan17994@gmail.com>
2021-03-15 22:45:48 +0200

    Add a new route - auth

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoute from './routes/auth.js';
 
 const app = express();
 
@@ -8,6 +9,8 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(authRoute);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
