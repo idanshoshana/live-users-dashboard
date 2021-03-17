11a4f0981efba22922634734c36e6ccab0aea1fa
Idan Shoshana <idan17994@gmail.com>
2021-03-17 23:01:59 +0200

    Navigate to dashboard page when user is logged in

--- client/src/components/login-form/login-form.js
+++ client/src/components/login-form/login-form.js
@@ -1,15 +1,18 @@
 import { loginSuccess, updateToken } from '../../store/auth/auth.actions';
+import { useHistory } from 'react-router-dom';
 import { useAuthContext } from '../../store/auth/auth.context';
 import { loginRequest } from '../../utils/authRequests';
 import BasicForm from '../basic-form';
 
 const LoginForm = () => {
+  const history = useHistory();
   const { dispatch } = useAuthContext();
 
   const login = async ({ username, password }) => {
     const { accessToken } = await loginRequest({ username, password });
     dispatch(updateToken(accessToken));
     dispatch(loginSuccess(username));
+    history.push('/dashboard');
   };
 
   return <BasicForm title={'Login'} onSubmit={login} />;
--- client/src/components/sign-up-form/sign-up-form.js
+++ client/src/components/sign-up-form/sign-up-form.js
@@ -1,9 +1,11 @@
+import { useHistory } from 'react-router-dom';
 import { loginSuccess, updateToken } from '../../store/auth/auth.actions';
 import { useAuthContext } from '../../store/auth/auth.context';
 import { signUpRequest, loginRequest } from '../../utils/authRequests';
 import BasicForm from '../basic-form';
 
 const SignUpForm = () => {
+  const history = useHistory();
   const { dispatch } = useAuthContext();
 
   const signUp = async ({ username, password }) => {
@@ -11,6 +13,7 @@ const SignUpForm = () => {
     const { accessToken } = await loginRequest({ username, password });
     dispatch(updateToken(accessToken));
     dispatch(loginSuccess(username));
+    history.push('/dashboard');
   };
 
   return <BasicForm title={'Sign Up'} onSubmit={signUp} />;
