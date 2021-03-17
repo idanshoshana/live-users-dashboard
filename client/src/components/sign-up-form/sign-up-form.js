f00aea349bb161569c99d4ebc92a1c7bbf9da815
Idan Shoshana <idan17994@gmail.com>
2021-03-17 22:30:36 +0200

    Add signUp function to SignUpForm

--- client/src/components/sign-up-form/sign-up-form.js
+++ client/src/components/sign-up-form/sign-up-form.js
@@ -1,7 +1,17 @@
+import { loginSuccess, updateToken } from '../../store/auth/auth.actions';
+import { useAuthContext } from '../../store/auth/auth.context';
+import { signUpRequest, loginRequest } from '../../utils/authRequests';
 import BasicForm from '../basic-form';
 
 const SignUpForm = () => {
-  const signUp = (values) => {};
+  const { dispatch } = useAuthContext();
+
+  const signUp = async ({ username, password }) => {
+    await signUpRequest({ username, password });
+    const { accessToken } = await loginRequest({ username, password });
+    dispatch(updateToken(accessToken));
+    dispatch(loginSuccess(username));
+  };
 
   return <BasicForm title={'Sign Up'} onSubmit={signUp} />;
 };
