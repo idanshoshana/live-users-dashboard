cabaa23cf12bee720d7d511e8630ed6ed86900c5
Idan Shoshana <idan17994@gmail.com>
2021-03-17 18:26:24 +0200

    Add a new component called SignUpModal

---
+++ client/src/components/modal/sign-up-modal/sign-up-modal.js
@@ -0,0 +1,12 @@
+import SignUpForm from '../../sign-up-form';
+import Modal from '../modal';
+
+const SignUpModal = () => {
+  return (
+    <Modal>
+      <SignUpForm />
+    </Modal>
+  );
+};
+
+export default SignUpModal;
