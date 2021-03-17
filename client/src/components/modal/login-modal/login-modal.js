1409d9fdfe1766cb964cc71552a6034d6abd2af8
Idan Shoshana <idan17994@gmail.com>
2021-03-17 18:25:53 +0200

    Add a new component called LoginModal

---
+++ client/src/components/modal/login-modal/login-modal.js
@@ -0,0 +1,12 @@
+import LoginForm from '../../login-form';
+import Modal from '../modal';
+
+const LoginModal = () => {
+  return (
+    <Modal>
+      <LoginForm />
+    </Modal>
+  );
+};
+
+export default LoginModal;
