cb36287714435e947e2a3ce62c1843ab681e98d9
Idan Shoshana <idan17994@gmail.com>
2021-03-17 22:22:01 +0200

    Add login function to LoginForm component

--- client/src/components/login-form/login-form.js
+++ client/src/components/login-form/login-form.js
@@ -1,9 +1,18 @@
+import { loginSuccess, updateToken } from '../../store/auth/auth.actions';
+import { useAuthContext } from '../../store/auth/auth.context';
+import { loginRequest } from '../../utils/authRequests';
 import BasicForm from '../basic-form';
 
 const LoginForm = () => {
-  const logIn = (values) => {};
+  const { dispatch } = useAuthContext();
 
-  return <BasicForm title={'Login'} onSubmit={logIn} />;
+  const login = async ({ username, password }) => {
+    const { accessToken } = await loginRequest({ username, password });
+    dispatch(updateToken(accessToken));
+    dispatch(loginSuccess(username));
+  };
+
+  return <BasicForm title={'Login'} onSubmit={login} />;
 };
 
 export default LoginForm;
