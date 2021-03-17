2b5542a6163f78b001a8acc0053e713566a7d55d
Idan Shoshana <idan17994@gmail.com>
2021-03-17 22:21:18 +0200

    Add posts fetching in Dashboard page

--- client/src/pages/dashboard/dashboard.js
+++ client/src/pages/dashboard/dashboard.js
@@ -1,21 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Posts from '../../components/posts';
 import Header from '../../components/header';
 import { Title, Wrapper, Button } from './dashboard.style';
 import CreatePostModal from '../../components/modal/create-post-modal';
+import { useAuthContext } from '../../store/auth/auth.context';
+import { useDashboardContext } from '../../store/dashboard/dashboard.context';
+import { fetchPostsRequest } from '../../utils/dashboardRequests';
+import { fetchPostsSuccess } from '../../store/dashboard/dashboard.actions';
 
 const Dashboard = () => {
+  const {
+    auth: { username, token },
+  } = useAuthContext();
+  const {
+    dashboard: { posts },
+    dispatch,
+  } = useDashboardContext();
   const [createPostDialogOpened, setCreatePostDialogOpened] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      if (token) {
+        const posts = await fetchPostsRequest(token);
+        dispatch(fetchPostsSuccess(posts));
+      }
+    };
+    fetchPosts();
+  }, [token, dispatch]);
 
   return (
     <>
       <Header />
       <Wrapper>
-        <Title>Welcome Idan!</Title>
+        <Title>{username}</Title>
         <Button onClick={() => setCreatePostDialogOpened(true)}>
           New Post
         </Button>
-        <Posts posts={[{ title: 'hello', content: 'hhh' }]} />
+        <Posts posts={posts} />
       </Wrapper>
 
       {createPostDialogOpened && (
